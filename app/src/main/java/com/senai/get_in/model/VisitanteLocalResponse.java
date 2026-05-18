6e8a1feb0257b987a5ab07fd37d6f826a179505a
Z3rOoO <henrique.cosmemk4@gmail.com>
2026-05-18 16:42:51 -0300

    Revert "fix: padronização de modelos JSON e melhoria nos logs de erro da API"
    This reverts commit d9defab60d682dbe01a16fde21c50c14813b811e.

--- app/src/main/java/com/senai/get_in/model/VisitanteLocalResponse.java
+++ app/src/main/java/com/senai/get_in/model/VisitanteLocalResponse.java
@@ -1,44 +1,11 @@
 package com.senai.get_in.model;
 
-import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class VisitanteLocalResponse {
-    @SerializedName("sucesso")
     private boolean sucesso;
+    private List<VisitanteLocal> dados;
 
-    @SerializedName("mensagem")
-    private String mensagem;
-
-    @SerializedName("data")
-    private List<VisitanteLocal> data;
-
-    public boolean isSucesso() {
-        return sucesso;
-    }
-
-    public void setSucesso(boolean sucesso) {
-        this.sucesso = sucesso;
-    }
-
-    public String getMensagem() {
-        return mensagem;
-    }
-
-    public void setMensagem(String mensagem) {
-        this.mensagem = mensagem;
-    }
-
-    public List<VisitanteLocal> getData() {
-        return data;
-    }
-
-    public void setData(List<VisitanteLocal> data) {
-        this.data = data;
-    }
-
-    // Mantendo compatibilidade com o código antigo que chamava getDados()
-    public List<VisitanteLocal> getDados() {
-        return data;
-    }
+    public boolean isSucesso() { return sucesso; }
+    public List<VisitanteLocal> getDados() { return dados; }
 }
