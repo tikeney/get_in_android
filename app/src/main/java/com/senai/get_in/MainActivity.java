2bcb4753d3249aaf67d2daccc229216345e7dbb7
tikeney <2.09538288e+08+tikeney@users.noreply.github.com>
2026-05-30 23:06:32 +0000

    Revert "style: aplica 70% de transparencia e efeito de blur (RenderEffect) na toolbar"
    This reverts commit f10ed7f56b8a981615b80ad53155f37ad2cbf810.

--- app/src/main/java/com/senai/get_in/MainActivity.java
+++ app/src/main/java/com/senai/get_in/MainActivity.java
@@ -4,12 +4,9 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.graphics.RenderEffect;
-import android.graphics.Shader;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -161,18 +158,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         });
 
         updateNavHeader(currentUser);
-        applyToolbarBlur();
     }
 
-    private void applyToolbarBlur() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            binding.toolbar.setRenderEffect(
-                RenderEffect.createBlurEffect(20f, 20f, Shader.TileMode.CLAMP)
-            );
-        }
-    }
-
-    public void applyBottomNavConfig() {
+    private void setupNavigation() {
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host_fragment);
         navController = navHostFragment.getNavController();
